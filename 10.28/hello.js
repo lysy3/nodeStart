9a4e8c2e7786ec8a98989f7ec492bbcbf7f19a4c
MichalZ <michalzach69@gmail.com>
2024-10-28 10:12:55 +0100

    dodanie nowej funkci

--- 10.28/hello.js
+++ 10.28/hello.js
@@ -7,9 +7,23 @@
 // }, 1000)
 
 //pobieranie z pliku
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.readFile('example.txt', 'utf-8', (err, data) => {
-    if(err) console.log("Blad podczas odczytu pliku: ", err);
-    else console.log(data);
-})
+// fs.readFile('example.txt', 'utf-8', (err, data) => {
+//     if(err) console.log("Blad podczas odczytu pliku: ", err);
+//     else console.log(data);
+// })
+
+function addZero(num){
+    return (num < 10) ? '0' + num : num
+}
+
+setInterval( ()=> {
+    let date = new Date()
+    console.clear()
+    console.log(    
+        addZero(date.getHours()) + ":" + 
+        addZero(date.getMinutes()) + ":" + 
+        addZero(date.getSeconds())
+    );
+},1000)
